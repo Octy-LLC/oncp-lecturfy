d9e16b979a29f7a0f9ee8db215a85da277ea8812
DeLitX <dimkanan@gmail.com>
2024-10-29 17:08:33 +0200

    Improved upload file screen ui

--- app/src/main/java/com/lecturfy/components/DropdownList.kt
+++ app/src/main/java/com/lecturfy/components/DropdownList.kt
@@ -1,17 +1,28 @@
 package com.lecturfy.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.focusable
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,15 +30,12 @@ fun DropdownList(
     options: Array<String>,
     selected: String,
     onSelectionChange: (String) -> Unit,
-    textFieldColors: TextFieldColors = TextFieldDefaults.textFieldColors(
-        containerColor = Color.White,
-        disabledTextColor = Color.Gray,
-        focusedTextColor = Color.Black,
-        unfocusedTextColor = Color.Black
-    ),
+    modifier: Modifier = Modifier,
+    textFieldColors: TextFieldColors = OutlinedTextFieldDefaults.colors(),
+    shape: Shape = OutlinedTextFieldDefaults.shape,
     dropdownItemTextColor: Color = Color.Black,
     dropdownBackgroundColor: Color = Color.White,
-    textFontSize: TextUnit = 16.sp,
+    textStyle: TextStyle = LocalTextStyle.current,
     itemsFontSize: Int = 16,
 ) {
     var expanded by remember { mutableStateOf(false) }
@@ -36,16 +44,20 @@ fun DropdownList(
         ExposedDropdownMenuBox(
             expanded = expanded,
             onExpandedChange = { expanded = !expanded },
-            modifier = Modifier.fillMaxWidth()
+            modifier = modifier
         ) {
-            TextField(
+            OutlinedTextField(
                 value = selected,
                 onValueChange = {},
                 readOnly = true,
                 colors = textFieldColors,
+                shape = shape,
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                modifier = Modifier.menuAnchor().focusable(false),
-                textStyle = TextStyle(fontSize = textFontSize),
+                modifier = Modifier
+                    .menuAnchor()
+                    .focusable(false)
+                    .fillMaxWidth(),
+                textStyle = textStyle,
             )
 
             ExposedDropdownMenu(
@@ -55,7 +67,13 @@ fun DropdownList(
             ) {
                 options.forEach { item ->
                     DropdownMenuItem(
-                        text = { Text(text = item, fontStyle = FontStyle(itemsFontSize), color = dropdownItemTextColor) },
+                        text = {
+                            Text(
+                                text = item,
+                                fontStyle = FontStyle(itemsFontSize),
+                                color = dropdownItemTextColor
+                            )
+                        },
                         onClick = {
                             onSelectionChange(item)
                             expanded = false
