0f482cdcc343ea92fa22f56232e2d95fe276d690
DeLitX <dimkanan@gmail.com>
2024-10-30 09:21:57 +0200

    Improved uploaded files screen ui

--- app/src/main/java/com/lecturfy/screens/OverlayUploadedFilesTopBarScreen.kt
+++ app/src/main/java/com/lecturfy/screens/OverlayUploadedFilesTopBarScreen.kt
@@ -1,60 +1,50 @@
 package com.lecturfy.screens
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.ui.Alignment
-import androidx.compose.material3.Button
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.background
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import com.lecturfy.components.text.AnnotatedText
-import com.lecturfy.components.text.AnnotatedTextPart
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.material.icons.filled.Add
-import androidx.navigation.NavHostController
-import android.content.ContextWrapper
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.media3.common.util.UnstableApi
-import android.annotation.SuppressLint
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import android.content.SharedPreferences
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import kotlinx.coroutines.launch
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import kotlinx.coroutines.CoroutineScope
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.media3.common.util.UnstableApi
+import androidx.navigation.NavHostController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.lecturfy.components.google.GoogleSignInHelper
 import com.lecturfy.components.google.launchGoogleSignIn
-import com.lecturfy.components.*
 import com.lecturfy.components.system.NavController
 import com.lecturfy.components.system.NavControllerViewModelDirectionEnum
 import com.lecturfy.components.system.SnackbarEntity
+import com.lecturfy.components.text.AnnotatedText
+import com.lecturfy.components.text.AnnotatedTextPart
+import com.lecturfy.theme.AppTheme
 import com.lecturfy.viewmodels.OverlayuploadedfilestopbarViewModel
-import com.lecturfy.R
-import com.lecturfy.components.utils.getSavedLocale
-import com.lecturfy.components.utils.saveLocale
-import com.lecturfy.components.utils.updateLocale
-import com.lecturfy.components.utils.FileUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @androidx.annotation.OptIn(UnstableApi::class)
 @OptIn(ExperimentalMaterial3Api::class)
@@ -66,7 +56,7 @@ fun OverlayuploadedfilestopbarScreen(
     coroutineScope: CoroutineScope,
     drawerState: DrawerState,
     viewModel: OverlayuploadedfilestopbarViewModel = hiltViewModel(),
-    ){
+) {
 
     val state by viewModel.state.collectAsState()
     val context = LocalContext.current
@@ -84,8 +74,8 @@ fun OverlayuploadedfilestopbarScreen(
     LaunchedEffect(Unit) {
         viewModel.init()
     }
-    
-    
+
+
 
     LaunchedEffect(state.googleSignInRequest) {
         state.googleSignInRequest?.let {
@@ -112,9 +102,9 @@ fun OverlayuploadedfilestopbarScreen(
     LaunchedEffect(state.navController) {
         state.navController?.let { ent: NavController ->
             coroutineScope.launch {
-                if(ent.direction == NavControllerViewModelDirectionEnum.TO){
+                if (ent.direction == NavControllerViewModelDirectionEnum.TO) {
                     navController.navigate(ent.path)
-                } else if(ent.direction == NavControllerViewModelDirectionEnum.BACK){
+                } else if (ent.direction == NavControllerViewModelDirectionEnum.BACK) {
                     navController.navigateUp()
                 }
                 viewModel.clearNavController()
@@ -122,108 +112,63 @@ fun OverlayuploadedfilestopbarScreen(
         }
     }
 
-    
-Row(
-    modifier = 
-Modifier
-        
-        .fillMaxWidth(1f)
-,
-    horizontalArrangement = Arrangement.SpaceBetween,
-    verticalAlignment = Alignment.CenterVertically,
-) {
-    
-    
-    
-    Button(
-        onClick = label@{ 
-        coroutineScope.launch {
-        
-        navController.navigateUp()
-        
-        }
-         },
-        modifier = 
-    Modifier
-    ,
-        
-        colors = ButtonDefaults.buttonColors(
-          containerColor = Color(android.graphics.Color.parseColor("#FFFFFF")), 
-          
-          
-          
-        ),
-        elevation = ButtonDefaults.buttonElevation(
-          
-          
-          
-          
-          
-        ),) {
-        
-        Icon(
-          imageVector = Icons.Filled.ArrowBack,
-          contentDescription = "", 
-          tint = Color(android.graphics.Color.parseColor("#17191D")),
-          modifier = 
-        Modifier
-        
-        )
-    }    
-    
-    AnnotatedText(
-        parts = listOf(
-            AnnotatedTextPart.TextPart(
-      "Uploaded files"
-    )
-    
-        ),
-        modifier = 
-    Modifier
-    ,
-        
-        fontSize = 24.sp,
-        textAlign = TextAlign.Center,
-    )
-    
-    
-    
-    Button(
-        onClick = label@{ 
-        coroutineScope.launch {
-        
-        navController.navigate(
-            "UploadFile"
-        )
-        }
-         },
-        modifier = 
-    Modifier
-    ,
-        
-        colors = ButtonDefaults.buttonColors(
-          containerColor = Color(android.graphics.Color.parseColor("#FFFFFF")), 
-          
-          
-          
-        ),
-        elevation = ButtonDefaults.buttonElevation(
-          
-          
-          
-          
-          
-        ),) {
-        
-        Icon(
-          imageVector = Icons.Filled.Add,
-          contentDescription = "", 
-          tint = Color(android.graphics.Color.parseColor("#17191D")),
-          modifier = 
-        Modifier
-        
-        )
-    }}
 
-    
+    Row(
+        modifier =
+        Modifier
+            .background(AppTheme.colors.accentColor1)
+            .fillMaxWidth(1f),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "",
+            tint = Color(android.graphics.Color.parseColor("#17191D")),
+            modifier =
+            Modifier
+                .padding(start = 8.dp, top = 12.dp, bottom = 16.dp)
+                .clickable {
+                    coroutineScope.launch {
+                        navController.navigateUp()
+                    }
+                }
+                .padding(8.dp)
+                .size(24.dp)
+
+        )
+        AnnotatedText(
+            parts = listOf(
+                AnnotatedTextPart.TextPart(
+                    "Uploaded files"
+                )
+
+            ),
+            modifier =
+            Modifier,
+
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 28.dp
+        )
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "",
+            tint = Color(android.graphics.Color.parseColor("#17191D")),
+            modifier =
+            Modifier
+                .padding(end = 8.dp, top = 12.dp, bottom = 16.dp)
+                .clickable {
+                    coroutineScope.launch {
+                        navController.navigate(
+                            "UploadFile"
+                        )
+                    }
+                }
+                .padding(8.dp)
+                .size(24.dp)
+
+        )
+    }
 }
--- app/src/main/java/com/lecturfy/screens/UploadedFilesScreen.kt
+++ app/src/main/java/com/lecturfy/screens/UploadedFilesScreen.kt
@@ -1,59 +1,48 @@
 package com.lecturfy.screens
 
-import com.lecturfy.components.pulltorefresh.PullToRefreshLazyColumn
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.background
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.ButtonDefaults
-import com.lecturfy.theme.AppTheme
-import com.lecturfy.components.text.AnnotatedText
-import com.lecturfy.components.text.AnnotatedTextPart
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.navigation.NavHostController
-import android.content.ContextWrapper
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.media3.common.util.UnstableApi
-import android.annotation.SuppressLint
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import android.content.SharedPreferences
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import kotlinx.coroutines.launch
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import kotlinx.coroutines.CoroutineScope
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.media3.common.util.UnstableApi
+import androidx.navigation.NavHostController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.lecturfy.components.google.GoogleSignInHelper
 import com.lecturfy.components.google.launchGoogleSignIn
-import com.lecturfy.components.*
+import com.lecturfy.components.pulltorefresh.PullToRefreshLazyColumn
 import com.lecturfy.components.system.NavController
 import com.lecturfy.components.system.NavControllerViewModelDirectionEnum
 import com.lecturfy.components.system.SnackbarEntity
+import com.lecturfy.components.text.AnnotatedText
+import com.lecturfy.components.text.AnnotatedTextPart
+import com.lecturfy.theme.AppTheme
 import com.lecturfy.viewmodels.UploadedfilesViewModel
-import com.lecturfy.R
-import com.lecturfy.components.utils.getSavedLocale
-import com.lecturfy.components.utils.saveLocale
-import com.lecturfy.components.utils.updateLocale
-import com.lecturfy.components.utils.FileUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @androidx.annotation.OptIn(UnstableApi::class)
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,7 +54,7 @@ fun UploadedfilesScreen(
     coroutineScope: CoroutineScope,
     drawerState: DrawerState,
     viewModel: UploadedfilesViewModel = hiltViewModel(),
-    ){
+) {
 
     val state by viewModel.state.collectAsState()
     val context = LocalContext.current
@@ -83,8 +72,8 @@ fun UploadedfilesScreen(
     LaunchedEffect(Unit) {
         viewModel.init()
     }
-    
-    
+
+
 
     LaunchedEffect(state.googleSignInRequest) {
         state.googleSignInRequest?.let {
@@ -111,9 +100,9 @@ fun UploadedfilesScreen(
     LaunchedEffect(state.navController) {
         state.navController?.let { ent: NavController ->
             coroutineScope.launch {
-                if(ent.direction == NavControllerViewModelDirectionEnum.TO){
+                if (ent.direction == NavControllerViewModelDirectionEnum.TO) {
                     navController.navigate(ent.path)
-                } else if(ent.direction == NavControllerViewModelDirectionEnum.BACK){
+                } else if (ent.direction == NavControllerViewModelDirectionEnum.BACK) {
                     navController.navigateUp()
                 }
                 viewModel.clearNavController()
@@ -121,83 +110,76 @@ fun UploadedfilesScreen(
         }
     }
 
-    
-PullToRefreshLazyColumn(
-    modifier = 
-Modifier
-        
-        .background(Color(android.graphics.Color.parseColor("#FFFFFF")))
-,
-    onRefresh = label@{  },
-    content = {
-        
-        items(state.transcriptions) { model ->
-            
-            
-            
-            Button(
-                onClick = label@{ 
-                coroutineScope.launch {
-                
-                navController.navigate(
-                   "Transcription/${model.id}"
-                )
-                }
-                 },
-                modifier = 
-            Modifier
-                    
-                    .fillMaxWidth(1f)
-                    
-                    .padding(
-                        
-                        
-                        
-                        bottom = 4.dp,
-                        )
-            ,
-                
-                colors = ButtonDefaults.buttonColors(
-                  containerColor = AppTheme.colors.backgroundColor1, 
-                  
-                  
-                  
-                ),
-                elevation = ButtonDefaults.buttonElevation(
-                  
-                  
-                  
-                  
-                  
-                ),) {
-                
-                
-                AnnotatedText(
-                    parts = listOf(
-                        AnnotatedTextPart.TextPart(
-                  model.title
-                )
-                
-                    ),
-                    modifier = 
-                Modifier
-                        
-                        .padding(
-                            start = 16.dp,
-                            top = 10.dp,
-                            
-                            bottom = 10.dp,
-                            )
-                ,
-                    
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    textAlign = TextAlign.Left,
-                    lineHeight = 24.dp,
-                )
-            }}
-    }
-)
 
-    
+    PullToRefreshLazyColumn(
+        modifier =
+        Modifier
+
+            .background(Color(android.graphics.Color.parseColor("#FFFFFF"))),
+        onRefresh = label@{ },
+        content = {
+
+            items(state.transcriptions) { model ->
+
+
+                Button(
+                    onClick = label@{
+                        coroutineScope.launch {
+
+                            navController.navigate(
+                                "Transcription/${model.id}"
+                            )
+                        }
+                    },
+                    modifier =
+                    Modifier
+                        .fillMaxWidth(1f)
+                        .alpha(0.8f)
+                        .padding(
+                            horizontal = 16.dp,
+                        ),
+                    shape = RoundedCornerShape(8.dp),
+                    contentPadding = PaddingValues(),
+
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = AppTheme.colors.backgroundColor1,
+
+
+                        ),
+                    elevation = ButtonDefaults.buttonElevation(
+
+
+                    ),
+                ) {
+
+
+                    AnnotatedText(
+                        parts = listOf(
+                            AnnotatedTextPart.TextPart(
+                                model.title
+                            )
+
+                        ),
+                        modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                start = 16.dp,
+                                end = 16.dp,
+                                top = 10.dp,
+                                bottom = 10.dp,
+                            ),
+
+                        color = AppTheme.colors.headerColor2,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        textAlign = TextAlign.Start,
+                        lineHeight = 20.dp,
+                    )
+                }
+            }
+        }
+    )
+
+
 }
